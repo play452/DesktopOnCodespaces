0adb779c3d7c95d3e16a0622b11892eb74f0882e
play452 <oscarlin@edu.schools.us.kg>
2024-10-15 12:38:59 -0400

    allowed docker access

--- install-docker.sh
+++ install-docker.sh
@@ -12,9 +12,9 @@ cp -r DesktopOnCodespaces/root/config/* Save
 
 json_file="DesktopOnCodespaces/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
+    docker run -dit --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock desktoponcodespaces
 else
-    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
+    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock desktoponcodespaces
 fi
 clear
 echo "INSTALL FINISHED! it is on port 3000"
