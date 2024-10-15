c5d42ce6136bd32c6f415d19007c3ae85a092178
play452 <oscarlin@edu.schools.us.kg>
2024-10-15 12:35:27 -0400

    fixed discord

--- root/installable-apps/discord.sh
+++ root/installable-apps/discord.sh
@@ -1,7 +1,7 @@
 apt update
 apt install -y libatomic1
 
-wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
-dpkg -i discord.deb
+wget https://discord.com/api/download?platform=linux&format=deb
+dpkg -i discord-0.0.71
 sleep 1
-rm discord.deb
+rm discord-0.0.71
