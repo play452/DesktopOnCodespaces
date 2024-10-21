f0739596b77b5bd6d5632581771c90cafc527718
play452 <oscarlin@edu.schools.us.kg>
2024-10-21 12:25:48 -0400

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,4 @@
+apt install docker.io podman sudo python3 pip python-is-python3 -y
 pip install textual
 sleep 2
 python3 installer.py
@@ -12,9 +13,9 @@ cp -r DesktopOnCodespaces/root/config/* Save
 
 json_file="DesktopOnCodespaces/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -dit --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock desktoponcodespaces
+    docker run -dit --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped -v /var/run/podman/podman.sock:/var/run/podman/podman.sock -v /var/run/docker.sock:/var/run/docker.sock desktoponcodespaces
 else
-    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock desktoponcodespaces
+    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped -v /var/run/podman/podman.sock:/var/run/podman/podman.sock -v /var/run/docker.sock:/var/run/docker.sock desktoponcodespaces
 fi
 clear
 echo "INSTALL FINISHED! it is on port 3000"
