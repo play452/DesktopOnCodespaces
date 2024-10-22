29fde22d7e07d0f2697a4cee628c5983ffb76626
play452 <oscarlin@edu.schools.us.kg>
2024-10-22 13:19:40 -0400

    systemd in container

--- install-podman.sh
+++ install-podman.sh
@@ -15,9 +15,9 @@ cp -r DesktopOnCodespaces/root/config/* Save
 
 json_file="DesktopOnCodespaces/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    podman run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped -v /var/run/podman/podman.sock:/var/run/podman/podman.sock -v /var/run/docker.sock:/var/run/docker.sock localhost/desktoponcodespaces:latest
+    podman run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /run:/run -v /etc/machine-id:/etc/machine-id:ro --restart unless-stopped -v /var/run/podman/podman.sock:/var/run/podman/podman.sock -v /var/run/docker.sock:/var/run/docker.sock localhost/desktoponcodespaces:latest
 else
-    podman run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped -v /var/run/podman/podman.sock:/var/run/podman/podman.sock -v /var/run/docker.sock:/var/run/docker.sock localhost/desktoponcodespaces:latest
+    podman run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /run:/run -v /etc/machine-id:/etc/machine-id:ro --restart unless-stopped -v /var/run/podman/podman.sock:/var/run/podman/podman.sock -v /var/run/docker.sock:/var/run/docker.sock localhost/desktoponcodespaces:latest
 fi
 clear
 echo "INSTALL FINISHED! it is on port 3000"
